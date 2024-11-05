155dd9e2857b7f080c09ba071fbd3aaa17b13661
abhinesh <abhinesh.s@icanio.com>
2024-11-05 15:08:56 +0530

    gitignore updated

--- src/index.jsx
+++ src/index.jsx
@@ -29,7 +29,6 @@ import '@fontsource/public-sans/700.css';
 
 // project import
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -37,7 +36,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
