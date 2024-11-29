7335eb740e4a72aa3370b3514f05fb1f903c6194
abhinesh <abhinesh.s@icanio.com>
2024-11-29 12:24:32 +0530

    ProtectedRoutes added

---
+++ src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthCookies } from 'utils/cookie';
+
+const useAuth = () => {
+  const { getCookie } = useAuthCookies()
+
+  const token = getCookie("token");
+  return !!token;
+};
+
+const ProtectedRoute = ({ children }) => {
+  const isAuth = useAuth();
+
+  return isAuth ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
