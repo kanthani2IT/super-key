98c425878d00c18926bec0d0184cd3b035738ef7
abhinesh <abhinesh.s@icanio.com>
2024-11-29 12:27:22 +0530

    unwanted import removed

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuthCookies } from 'utils/cookie';
 
 const useAuth = () => {
