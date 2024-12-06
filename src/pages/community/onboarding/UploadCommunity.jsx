3ed7c41fb9eacbc26130b20c1e7136f98286e01e
kanthan-icanio <kanthan.m@icanio.com>
2024-12-06 14:39:13 +0530

    dynamic model route

--- src/pages/community/onboarding/UploadCommunity.jsx
+++ src/pages/community/onboarding/UploadCommunity.jsx
@@ -1,74 +1,36 @@
-import CustomUploadTable from "components/AppComponents/CustomUploadTable";
-
-const cols = [
-  { title: "Community Name", fieldName: "communityName" },
-  { title: "Community's Email", fieldName: "communityEmail" },
-  { title: "Contact No", fieldName: "contactNo" },
-  { title: "Insurance Status", fieldName: "insuranceStatus" },
-  { title: "Community Managers", fieldName: "communityManagers" },
-  { title: "Property Manager No", fieldName: "propertyManagerNo" },
-  { title: "Address", fieldName: "address" },
-  { title: "", fieldName: "action" },
-];
-
-const tableData = [
-  {
-    communityName: "Desert Eagle",
-    communityEmail: "eagle@gmail.com",
-    contactNo: "+1 4323456",
-    insuranceStatus: "Insured",
-    communityManagers: "Alex",
-    propertyManagerNo: "+1 4323456",
-    address: "Phoenix North EstatesNY 54321,USA",
-  },
-  {
-    communityName: "Desert Eagle",
-    communityEmail: "eagle@gmail.com",
-    contactNo: "+1 4323456",
-    insuranceStatus: "Insured",
-    communityManagers: "Alex",
-    propertyManagerNo: "+1 4323456",
-    address: "Phoenix North EstatesNY 54321,USA",
-  },
-  {
-    communityName: "Desert Eagle",
-    communityEmail: "eagle@gmail.com",
-    contactNo: "+1 4323456",
-    insuranceStatus: "Insured",
-    communityManagers: "Alex",
-    propertyManagerNo: "+1 4323456",
-    address: "Phoenix North EstatesNY 54321,USA",
-  },
-  {
-    communityName: "Desert Eagle",
-    communityEmail: "eagle@gmail.com",
-    contactNo: "+1 4323456",
-    insuranceStatus: "Insured",
-    communityManagers: "Alex",
-    propertyManagerNo: "+1 4323456",
-    address: "Phoenix North EstatesNY 54321,USA",
-  },
-  {
-    communityName: "Desert Eagle",
-    communityEmail: "eagle@gmail.com",
-    contactNo: "+1 4323456",
-    insuranceStatus: "Insured",
-    communityManagers: "Alex",
-    propertyManagerNo: "+1 4323456",
-    address: "Phoenix North EstatesNY 54321,USA",
-  },
-  {
-    communityName: "Desert Eagle",
-    communityEmail: "eagle@gmail.com",
-    contactNo: "+1 4323456",
-    insuranceStatus: "Insured",
-    communityManagers: "Alex",
-    propertyManagerNo: "+1 4323456",
-    address: "Phoenix North EstatesNY 54321,USA",
-  },
-];
-
+import { Button, Typography } from "@mui/material";
+import icons from "assets/images/icons/mui-icons/Icons";
+import AppGrid from "components/AppComponents/AppGrid";
+import InsuranceDocument from "components/AppComponents/UploadDocument";
 const UploadCommunity = () => {
-  return <CustomUploadTable cols={cols} tableData={tableData} />;
+  return (
+    <>
+      <AppGrid container spacing={5}>
+        <AppGrid size={{ xl: 6 }}>
+          <Typography variant="body1" color="#5B738B" gutterBottom>
+            Steps to bulk upload the communities{" "}
+          </Typography>
+          <Typography variant="body2" color="#5B738B" gutterBottom>
+            1.Please download the template{" "}
+          </Typography>
+          <Typography variant="body2" color="#5B738B" gutterBottom>
+            2.Upload the Details in the given template format{" "}
+          </Typography>
+          <Typography variant="body2" color="#5B738B" gutterBottom>
+            3.Upload the file{" "}
+          </Typography>
+        </AppGrid>
+        <AppGrid size={{ xl: 6 }} display={"flex"} alignItems={"center"}>
+          <Button variant="outlined" startIcon={icons.IconArrowDownward()}>
+            Download the Template
+          </Button>
+        </AppGrid>
+        <AppGrid size={{ xl: 12 }}>
+          <InsuranceDocument />
+        </AppGrid>
+      </AppGrid>
+    </>
+  );
 };
+
 export default UploadCommunity;
--- src/pages/community/onboarding/UploadCommunityList.jsx
+++ src/pages/community/onboarding/UploadCommunityList.jsx
@@ -1,36 +1,74 @@
-import { Button, Typography } from "@mui/material";
-import { Grid } from "@mui/system";
-import icons from "assets/images/icons/mui-icons/Icons";
-import InsuranceDocument from "components/AppComponents/UploadDocument";
-const UploadCommunityList = () => {
-  return (
-    <>
-      <Grid container spacing={5}>
-        <Grid size={{ xl: 6 }}>
-          <Typography variant="body1" color="#5B738B" gutterBottom>
-            Steps to bulk upload the communities{" "}
-          </Typography>
-          <Typography variant="body2" color="#5B738B" gutterBottom>
-            1.Please download the template{" "}
-          </Typography>
-          <Typography variant="body2" color="#5B738B" gutterBottom>
-            2.Upload the Details in the given template format{" "}
-          </Typography>
-          <Typography variant="body2" color="#5B738B" gutterBottom>
-            3.Upload the file{" "}
-          </Typography>
-        </Grid>
-        <Grid size={{ xl: 6 }} display={"flex"} alignItems={"center"}>
-          <Button variant="outlined" startIcon={icons.IconArrowDownward()}>
-            Download the Template
-          </Button>
-        </Grid>
-        <Grid size={{ xl: 12 }}>
-          <InsuranceDocument />
-        </Grid>
-      </Grid>
-    </>
-  );
-};
+import CustomUploadTable from "components/AppComponents/CustomUploadTable";
 
+const cols = [
+  { title: "Community Name", fieldName: "communityName" },
+  { title: "Community's Email", fieldName: "communityEmail" },
+  { title: "Contact No", fieldName: "contactNo" },
+  { title: "Insurance Status", fieldName: "insuranceStatus" },
+  { title: "Community Managers", fieldName: "communityManagers" },
+  { title: "Property Manager No", fieldName: "propertyManagerNo" },
+  { title: "Address", fieldName: "address" },
+  { title: "", fieldName: "action" },
+];
+
+const tableData = [
+  {
+    communityName: "Desert Eagle",
+    communityEmail: "eagle@gmail.com",
+    contactNo: "+1 4323456",
+    insuranceStatus: "Insured",
+    communityManagers: "Alex",
+    propertyManagerNo: "+1 4323456",
+    address: "Phoenix North EstatesNY 54321,USA",
+  },
+  {
+    communityName: "Desert Eagle",
+    communityEmail: "eagle@gmail.com",
+    contactNo: "+1 4323456",
+    insuranceStatus: "Insured",
+    communityManagers: "Alex",
+    propertyManagerNo: "+1 4323456",
+    address: "Phoenix North EstatesNY 54321,USA",
+  },
+  {
+    communityName: "Desert Eagle",
+    communityEmail: "eagle@gmail.com",
+    contactNo: "+1 4323456",
+    insuranceStatus: "Insured",
+    communityManagers: "Alex",
+    propertyManagerNo: "+1 4323456",
+    address: "Phoenix North EstatesNY 54321,USA",
+  },
+  {
+    communityName: "Desert Eagle",
+    communityEmail: "eagle@gmail.com",
+    contactNo: "+1 4323456",
+    insuranceStatus: "Insured",
+    communityManagers: "Alex",
+    propertyManagerNo: "+1 4323456",
+    address: "Phoenix North EstatesNY 54321,USA",
+  },
+  {
+    communityName: "Desert Eagle",
+    communityEmail: "eagle@gmail.com",
+    contactNo: "+1 4323456",
+    insuranceStatus: "Insured",
+    communityManagers: "Alex",
+    propertyManagerNo: "+1 4323456",
+    address: "Phoenix North EstatesNY 54321,USA",
+  },
+  {
+    communityName: "Desert Eagle",
+    communityEmail: "eagle@gmail.com",
+    contactNo: "+1 4323456",
+    insuranceStatus: "Insured",
+    communityManagers: "Alex",
+    propertyManagerNo: "+1 4323456",
+    address: "Phoenix North EstatesNY 54321,USA",
+  },
+];
+
+const UploadCommunityList = () => {
+  return <CustomUploadTable cols={cols} tableData={tableData} />;
+};
 export default UploadCommunityList;
