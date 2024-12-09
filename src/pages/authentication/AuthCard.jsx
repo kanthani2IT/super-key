ae3a0a02d736bff3052b1f00970787bfa9aace54
abhinesh s <abhinesh.s@icanio.com>
2024-12-09 12:15:20 +0530

    Login reDesign

--- src/pages/authentication/AuthCard.jsx
+++ src/pages/authentication/AuthCard.jsx
@@ -11,11 +11,12 @@ import MainCard from 'components/MainCard';
 export default function AuthCard({ children, ...other }) {
   return (
     <MainCard
-      sx={{ maxWidth: { xs: 400, lg: 475 }, margin: { xs: 2.5, md: 3 }, '& > *': { flexGrow: 1, flexBasis: '50%' } }}
+      background={false}
+      sx={{ maxWidth: { xs: 400, lg: 430 }, margin: { xs: 2.5, md: 3 }, '& > *': { flexGrow: 1, flexBasis: '50%' } }}
       content={false}
       {...other}
       border={false}
-      boxShadow
+      boxShadow={false}
       shadow={(theme) => theme.customShadows.z1}
     >
       <Box sx={{ p: { xs: 2, sm: 3, md: 4, xl: 5 } }}>{children}</Box>
