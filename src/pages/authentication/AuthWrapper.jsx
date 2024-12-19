cf8042a8ff0cd1d5bce609b8544697f3040bfe4e
abhinesh s <abhinesh.s@icanio.com>
2024-12-19 12:34:47 +0530

    s

--- src/pages/authentication/AuthWrapper.jsx
+++ src/pages/authentication/AuthWrapper.jsx
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types';
 // material-ui
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import loginBanner from 'assets/images/login/loginBanner.jpeg'
+import logo from 'assets/images/login/logoIcon.svg'
 // project import
 import AuthCard from './AuthCard';
+import { Image } from 'components/StyledComponents';
+import { Fab } from '@mui/material';
 export default function AuthWrapper({ children }) {
   return (
     <Box minHeight={'100vh'}>
 
-      <Stack minHeight={'100vh'} alignItems={'center'} justifyContent="center">
-        <Stack justifyContent={'center'} alignItems={'center'} justifySelf={'center'} justifyItems={'center'}>
+      <Stack flexDirection={'row'} justifyContent="space-between">
+        <Stack width={'50%'} justifyContent={'center'} maxHeight={'100vh'} alignItems={'center'} justifySelf={'center'} justifyItems={'center'}>
           <AuthCard>{children}</AuthCard>
         </Stack>
-        {/* <Image width={'50%'} height={'100vh'} src={loginBanner} /> */}
+        <Image width={'50%'} height={'100vh'} src={loginBanner} />
       </Stack>
-      {/* <Fab disabled sx={{ position: "absolute", top: "50%", right: "49%" }}>
+      <Fab disabled sx={{ position: "absolute", top: "50%", right: "49%" }}>
         <Image height={'75'} width={'80'} src={logo} />
-      </Fab> */}
+      </Fab>
     </Box>
   );
 }
